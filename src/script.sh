bb449dca22560ce583d92cefe3c5bdc96b1d3caa
shahzad <shahzadbinshahjahan@gmail.com>
2022-08-21 21:18:04 +0530

    added comments to src/script.sh

--- src/script.sh
+++ src/script.sh
@@ -3,7 +3,9 @@
 echo -n "Please enter ppt name"
 echo
 read pptname
+
 mkdir tempoutput/
+#copying the presentation from the input path to temporary folder
 cp  $1 tempoutput/
 cd tempoutput/
 mkdir images/
@@ -13,7 +15,8 @@ echo "Doing final touches ..."
 cd images/
 convert ../$pptname.pdf i.png
 
-#converting images to numbers for listing in python
+#depreceated - converting images to numbers for listing in python
+#as linux is not listing in numerical order
 
 #num=1
 #for file in *
@@ -21,13 +24,16 @@ convert ../$pptname.pdf i.png
 #	num=$((num+1))
 #done
 
+#converting image names to only number for listing in python
+#input each file in whatever order they are in
+#removes the first two character i.e "i-"
+#reverses it , i.e "10.png" wil be "gnp.01"
+#removes the first 4 chars from the reversed string, output: 01"
+#reverse it again to get the proper number, i.e: 01 will be 10"
 for file in *.png;
 do mv $file "$(echo $file | cut -c 3- | rev | cut -c 5- | rev)"
 done
 
-#clearing all tempfiles
-cd ../..
-#rm -r tempoutput/
 echo "Thank you for your patience"
 echo "Loading presentation"
 python3 main.py "tempoutput/images/"
